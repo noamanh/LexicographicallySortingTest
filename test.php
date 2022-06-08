dce95065c1600a7e7e803be654486b7f7fa933ec
Noaman Ahmed <nauman.h@allshorevirtualstaffing.com>
2022-06-08 20:00:47 +0500

    Add udiff function to fix case sensitive issue

--- test.php
+++ test.php
@@ -1,6 +1,18 @@
 #!/usr/bin/php
 <?php
 
+// Check if file exists or not 
+if(!file_exists('./input_1.txt'))
+{
+    exit("Error: Input File 1 Not found");
+}
+
+
+if(!file_exists('./input_2.txt'))
+{
+    exit("Error: Input File 2 Not found");
+}
+
 // Take input 
 $file_1 = file_get_contents('./input_1.txt');
 $file_2 = file_get_contents('./input_2.txt');
@@ -18,8 +30,8 @@ natcasesort($file_1_array);
 natcasesort($file_2_array);
 
 // Remove matching elements found in another array
-$new_file_1_array = array_diff($file_1_array,$file_2_array);
-$new_file_2_array = array_diff($file_2_array,$file_1_array);
+$new_file_1_array = array_udiff($file_1_array,$file_2_array,'strcasecmp');
+$new_file_2_array = array_udiff($file_2_array,$file_1_array,'strcasecmp');
 
 // Converting the filtered and sorted items into text
 $new_file_1 = implode("\n",$new_file_1_array);
